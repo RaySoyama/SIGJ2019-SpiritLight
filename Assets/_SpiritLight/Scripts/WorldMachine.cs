2859668f72c6d0b2186721f223c968f9c1518f9e
Ray Soyama <raysoyama@gmail.com>
2019-11-09 01:28:03 -0800

    Long Hallway, and world machine

---
+++ Assets/_SpiritLight/Scripts/WorldMachine.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMachine : MonoBehaviour
+{
+    public static WorldMachine World;
+
+    public enum GAMESTATE
+    {
+        Menu,
+        HallOne,
+        Tar,
+        HallTwo,
+        Boss,
+        End
+    }
+
+    public GameObject player;
+
+    public GAMESTATE CurrentGameState;
+
+    [SerializeField]
+    private bool isInRealm;
+    public bool IsInRealm
+    {
+        get
+        {
+            return isInRealm;
+        }
+    }
+
+    [SerializeField]
+    private Vector3 realmOffset;
+
+
+
+    void Start()
+    {
+        if (World == null)
+        {
+            World = this;
+        }
+        else
+        {
+            return;
+        }
+
+
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            EnterRealm();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ExitRealm();
+        }
+
+    }
+
+    public void EnterRealm()
+    {
+        isInRealm = true;
+        player.transform.position += realmOffset;
+
+    }
+
+    public void ExitRealm()
+    {
+        isInRealm = false;
+        player.transform.position -= realmOffset;
+
+    }
+    public void OnEnter()
+    { 
+
+    }
+
+
+
+}
